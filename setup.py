a07b23bd1b6127c0f9270c4bc4513eca2d82941d
Yann VERCUCQUE <yann.vercucque@orange.com>
2023-07-25 02:21:19 +0200

    clean setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@
 # -*- coding:utf-8 -*-
 """Packaging Setup."""
 from setuptools import setup
-import os
-import datetime
 
 if __name__ == "__main__":
-    now = datetime.datetime.now()
-    date_as_version = str(now.strftime("%Y.%m.%d.%H.%M.%S"))
-    version = os.environ.get("TAG_VERSION")
-    version = version if version else date_as_version
-    setup(version=version)
+    setup()
