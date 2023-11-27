321c3312f2863b3ae95d46a49419d1de37bcbf40
Yann VERCUCQUE <yann.vercucque@orange.com>
2023-11-27 11:37:21 +0100

    add fallback to setup a package even without tag

--- setup.py
+++ setup.py
@@ -2,9 +2,19 @@
 # -*- coding:utf-8 -*-
 """Packaging Setup."""
 from setuptools import setup
+import os
+import datetime
 
 if __name__ == "__main__":
-    setup(
-        use_scm_version=True,
-        setup_requires=['setuptools_scm'],
-    )
+    try:
+        setup(
+            use_scm_version=True,
+            setup_requires=["setuptools_scm"],
+        )
+    except Exception:
+        now = datetime.datetime.now()
+        date_as_version = str(now.strftime("%Y.%m.%d.%H.%M.%S"))
+
+        version = os.environ.get("CI_COMMIT_TAG")
+        version = version if version else date_as_version
+        setup(version=version)
